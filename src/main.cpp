d86f78f320d17c96f4c3aab290199abd95494199
MrDaniel307 <daniilox415@gmail.com>
2026-06-29 02:39:18 +0500

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,68 +1,50 @@
 #include <Geode/Geode.hpp>
-#include <Geode/modify/EditorUI.hpp>
-#include <cocos2d.h>
+#include <Geode/modify/MenuLayer.hpp>
 
 using namespace geode::prelude;
 
-class $modify(MyEditorUI, EditorUI) {
-    bool init(LevelEditorLayer* editorLayer) {
-        if (!EditorUI::init(editorLayer)) return false;
+class MyPopup : public geode::Popup<> {
+protected:
+    bool setup() override {
+        this->setTitle("Моё меню");
+        return true;
+    }
+public:
+    static MyPopup* create() {
+        auto ret = new MyPopup();
+        if (ret->initAnchored(280.f, 180.f)) {
+            ret->autorelease();
+            return ret;
+        }
+        delete ret;
+        return nullptr;
+    }
+};
 
-        auto menu = this->getChildByID("edit-menu");
-        if (!menu) {
-            menu = this->getChildByID("delete-menu");
-        }
-        if (!menu) {
-            menu = m_buildModeMenu;
-        }
+class $modify(MenuLayer) {
+    bool init() {
+        if (!MenuLayer::init()) return false;
 
-        if (menu) {
-            auto sprite = CCPositionBonusSprite::createWithSpriteFrameName("GJ_plusBtn_001.png", 0.8f);
-            auto button = CCMenuItemSpriteExtra::create(
-                sprite,
-                this,
-                menu_selector(MyEditorUI::onAutoDecorate)
-            );
-            button->setID("auto-decorate-button");
-            menu->addChild(button);
-            menu->updateLayout();
-        }
+        auto btn = CCMenuItemSpriteExtra::create(
+            CircleButtonSprite::createWithSpriteFrameName(
+                "edit_ePlusBtn_001.png",
+                1.f,
+                CircleBaseColor::Green,
+                CircleBaseSize::Small
+            ),
+            this,
+            menu_selector(MenuLayerExt::onFloatBtn)
+        );
+
+        auto menu = CCMenu::create();
+        menu->addChild(btn);
+        menu->setPosition(35.f, 35.f);
+        this->addChild(menu, 10);
 
         return true;
     }
 
-    void onAutoDecorate(CCObject* sender) {
-        auto editorLayer = m_editorLayer;
-        if (!editorLayer) return;
-
-        auto selectedObjects = this->getSelectedObjects();
-        if (!selectedObjects || selectedObjects->count() == 0) {
-            FLAlertLayer::create("AutoDecorator", "Выделите хотя бы один объект!", "OK")->show();
-            return;
-        }
-
-        std::vector<GameObject*> objectsToDecorate;
-        CCObject* obj;
-        CCARRAY_FOREACH(selectedObjects, obj) {
-            auto gameObj = static_cast<GameObject*>(obj);
-            if (gameObj) {
-                objectsToDecorate.push_back(gameObj);
-            }
-        }
-
-        for (auto* baseObj : objectsToDecorate) {
-            CCPoint basePos = baseObj->getPosition();
-            int decorationID = 44; 
-            
-            auto decorObj = editorLayer->createObject(decorationID, basePos + CCPoint{0, 30.0f}, true);
-            if (decorObj) {
-                decorObj->m_editorLayer = editorLayer;
-                editorLayer->m_objects->addObject(decorObj);
-                decorObj->determineCheckColor();
-                decorObj->setObjectColor(decorObj->m_baseColor);
-            }
-        }
-
-        FLAlertLayer::create("AutoDecorator", "Декорирование успешно завершено!", "OK")->show();
+    void onFloatBtn(CCObject*) {
+        MyPopup::create()->show();
     }
 };
